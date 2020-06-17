1fb45832393bef5624cea18458a4fdff46db4e55
janumedia <janumedia@gmail.com>
2020-06-17 14:59:04 +0800

    update options

--- ssr-puppeteer.js
+++ ssr-puppeteer.js
@@ -4,11 +4,11 @@ module.exports = async function (params) {
     const start = Date.now();
     const browser = await puppeteer.launch({
         headless: true,
-        //args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });in39406383
     
     const page = await browser.newPage();
-    await page.goto('http://vue-web-components.netlify.com', {waitUntil: 'networkidle0'});
+    await page.goto('https://vue-web-components.netlify.app', {waitUntil: 'networkidle0'});
     let html = await page.content();
     
     await browser.close();
